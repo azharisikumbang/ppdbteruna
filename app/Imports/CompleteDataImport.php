eba7ad6a0134c6563995e582e082bbce539dbd0a
Kum <azharisaputrasikumbang@gmail.com>
2025-03-23 12:29:53 +0700

    fix: type interfaces cannot extended

--- app/Imports/CompleteDataImport.php
+++ app/Imports/CompleteDataImport.php
@@ -2,16 +2,17 @@
 namespace App\Imports;
 
 use App\Models\Registration;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-class CompleteDataImport extends ToModel
+class CompleteDataImport implements ToModel
 {
     public function model(array $row)
     {
         return new Registration([
-           'name'     => $row[0],
-           'email'    => $row[1],
-           'password' => Hash::make($row[2]),
+            'name' => $row[0],
+            'email' => $row[1],
+            'password' => Hash::make($row[2]),
         ]);
     }
 }
