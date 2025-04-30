8e50e03b8fc9fe1aff4bd4b22d1f183094588e66
Kum <azharisaputrasikumbang@gmail.com>
2025-05-01 03:25:53 +0700

    fix: old input not showing

--- resources/views/login/home.blade.php
+++ resources/views/login/home.blade.php
@@ -21,7 +21,7 @@
       </label>
       <input
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        name="email" type="email" placeholder="email" value="{{ (isset($email)) ? $email : '' }}" autofocus="autofocus">
+        name="email" type="email" placeholder="email" value="{{ old('email') }}" autofocus="autofocus">
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
